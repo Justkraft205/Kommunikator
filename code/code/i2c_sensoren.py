3d3fa06d77bae31a84d38d9e8a081d423a02f6e1
Justkraft205 <107878833+Justkraft205@users.noreply.github.com>
2026-03-08 19:06:43 +0100

    Refactor I2C sensor handling and improve error reporting

--- code/code/i2c_sensoren.py
+++ code/code/i2c_sensoren.py
@@ -4,13 +4,14 @@ import shared, board, adafruit_max1704x, time, smbus2
 def restarting_i2c(bus_id=1):
     try:
         shared.bus.close()
+        shared.SENSOREN = []
         time.sleep(0.5)
         shared.bus = smbus2.SMBus(bus_id)
         return True
     except Exception as e: return False
 
 
-def check_i2c_devices(sensor_list, bus_id=1):
+def check_i2c_devices(sensor_list):
     shared.i2c = board.I2C()
     print("Aktive Sensoren:", shared.SENSOREN)
     for name, address in sensor_list:
@@ -33,8 +34,8 @@ def read_sensors():
     teile = shared.skalas.split(",")
     shared.sensor_data = {}
     print("Hier kannst du eigene Hinzufügen")
-    ok= bme280(teile)
-    if not ok: print("fehler bei  bme280")
+    ok, name = bme280(teile)
+    if not ok: print(f"Fehler bei: {name}")
 #-----------------------------------------------------------------------------------------------------------------------
 
 
@@ -43,12 +44,12 @@ def bme280(teile):
         x = round(shared.bme280.humidity, 1)
         y = round(shared.bme280.pressure, 1)
         z = round(shared.bme280.temperature, 1)
-        print(dir(shared.bme280))
         shared.sensor_data.update({
             "Luftfeuchtigkeit": f"{eval(teile[5])} {teile[4]}",
             "Luftdruck": f"{eval(teile[3])} {teile[2]}",
-            "Temperatur": f"{eval(teile[1])} {teile[0]}"
-        })
-        return True
-    else: return False
+            "Temperatur": f"{eval(teile[1])} {teile[0]}"})
+        for s in ["Temperatur", "Luftdruck", "Luftfeuchtigkeit"]:
+            if s not in shared.header:shared.header.append(s)
+        return True, "BME280"
+    else: return False, "BME280"
 
