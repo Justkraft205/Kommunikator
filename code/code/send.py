b91ac537abb15884bcb364fc6a9b81e47c72a539
Justkraft205 <107878833+Justkraft205@users.noreply.github.com>
2026-03-13 17:26:39 +0100

    Modify senden function to accept additional parameters
    Enhanced the 'senden' function to support additional parameters for mode, channel, id1, and id2, allowing for flexible message sending options.

--- code/code/send.py
+++ code/code/send.py
@@ -1,9 +1,18 @@
 # -*- coding: latin-1 -*-
 import shared
 from lora_e220 import LoRaE220
-def senden(server_id, message):
+
+def senden(server_id, message,mode=None, channel=None, id1=None, id2=None):
     try:
-        shared.lora.send_transparent_message(f"{server_id}:{message}\n".encode('utf-8'))
-        print("Gesendet")
-    except Exception as e:
-        print("Fehler beim Senden:", e)
+        if not "x" in server_id:
+            shared.lora.send_transparent_message(f"{server_id}:{message}\n")  # nur str, kein .encode()
+            print(f"Sende transparent: {server_id}:{message}")
+            return True
+        else:
+            id1, id2 = server_id.split(":")
+            id1 = int(id1, 16)
+            id2 = int(id2, 16)
+            shared.lora.send_fixed_message(id1, id2, channel, f"fix:{message}".encode('utf-8'))
+            print(f"Sende fixed: fix:{message}")
+            return True
+    except Exception as e: return False
