946fac390140e15f698968b33ab8a3621790b313
Justkraft205 <107878833+Justkraft205@users.noreply.github.com>
2026-03-26 15:29:25 +0100

    Update send.py

--- code/code/send.py
+++ code/code/send.py
@@ -2,17 +2,20 @@
 import shared
 from lora_e220 import LoRaE220
 
-def senden(server_id, message,mode=None, channel=None, id1=None, id2=None):
+def senden(server_id, message, channel):
     try:
-        if not "x" in server_id:
+        print("senden aktiviert")
+        print(server_id)
+        if not ":" in server_id:
             shared.lora.send_transparent_message(f"{server_id}:{message}\n")  # nur str, kein .encode()
             print(f"Sende transparent: {server_id}:{message}")
             return True
         else:
             id1, id2 = server_id.split(":")
-            id1 = int(id1, 16)
-            id2 = int(id2, 16)
-            shared.lora.send_fixed_message(id1, id2, channel, f"fix:{message}".encode('utf-8'))
-            print(f"Sende fixed: fix:{message}")
+            shared.lora.send_fixed_message(int(id1), int(id2), channel, message)
+            print(f"Sende fixed:{message}")
             return True
-    except Exception as e: return False
+    except Exception as e:
+        print(e)
+        print("Fehler beim senden in send.py")
+        return False
