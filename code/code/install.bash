a0311cf41654f80bc7146b1d1c2586953c58ee18
Justkraft205 <107878833+Justkraft205@users.noreply.github.com>
2026-04-18 09:59:20 +0200

    Dieses skript übernimmt die Arbeit zum Installieren aller Libarys

---
+++ code/code/install.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Aktualisiere Paketliste..."
+sudo apt update
+echo "Installiere pip (falls nicht vorhanden)..."
+sudo apt install -y python3-pip
+echo "Installiere Python Libraries..."
+pip3 install flask --break-system-packages
+echo "Flask Installiert"
+pip3 install pyserial --break-system-packages
+echo "serial Installiert"
+pip3 install adafruit-blinka smbus2 --break-system-packages
+echo "blinka-smbus2 Installiert"
+pip3 install adafruit-circuitpython-max1704x --break-system-packages
+echo "MAX17048 Installiert"
+pip3 install pytz --break-system-packages
+echo "pytz Installiert"
+pip3 install "mpmath==1.3.0" --break-system-packages
+echo "mpmath Installiert"
+pip3 install adafruit-circuitpython-bme280 --break-system-packages
+echo "BME280 Installiert"
+pip3 install ebyte-lora-e220-rpi --break-system-packages
+echo "LORA Installiert"
+sudo apt install ttyd
+echo "ttyd Installiert"
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_serial 0  # UART aktivieren
+sudo raspi-config nonint do_onewire 0
+echo "Fertig!"
